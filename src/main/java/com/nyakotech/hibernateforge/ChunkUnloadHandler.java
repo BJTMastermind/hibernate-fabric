ad2bbac2e609493d193faeabaeda80db6dc47415
Thadah D. Denyse <thadahdenyse@protonmail.com>
2024-10-23 20:57:30 +0200

    Unload chunks

---
+++ src/main/java/com/nyakotech/hibernateforge/ChunkUnloadHandler.java
@@ -0,0 +1,43 @@
+package com.nyakotech.hibernateforge;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.TicketType;
+import net.minecraft.util.Unit;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.ServerLifecycleHooks;
+import net.minecraftforge.fml.LogicalSide;
+
+@Mod.EventBusSubscriber(modid = HibernateForge.MODID)
+public class ChunkUnloadHandler {
+
+    private static boolean chunksUnloaded = false;
+
+    @SubscribeEvent
+    public static void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.side != LogicalSide.SERVER || event.phase != TickEvent.Phase.END) {
+            return;
+        }
+
+        if (HibernateForge.isHibernating() && !chunksUnloaded) {
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            for (ServerLevel level : server.getAllLevels()) {
+                // Remove the spawn chunk ticket
+                ChunkPos spawnChunk = new ChunkPos(level.getSharedSpawnPos());
+                level.getChunkSource().removeRegionTicket(TicketType.START, spawnChunk, 11, Unit.INSTANCE);
+            }
+            chunksUnloaded = true;
+        } else if (!HibernateForge.isHibernating() && chunksUnloaded) {
+            // Re-add the spawn chunk ticket when hibernation ends
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            for (ServerLevel level : server.getAllLevels()) {
+                ChunkPos spawnChunk = new ChunkPos(level.getSharedSpawnPos());
+                level.getChunkSource().addRegionTicket(TicketType.START, spawnChunk, 11, Unit.INSTANCE);
+            }
+            chunksUnloaded = false;
+        }
+    }
+}
