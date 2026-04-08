6f347f253c2030662dc6695a0a7112f1206191ca
BJTMastermind <bjtmastermind@gmail.com>
2026-04-08 19:55:35 -0400

    Added shutdown to scheduler used for the disconnect event, fixed shutdown for memory optimization not triggering on server shutdown. Fixes #11

--- src/main/java/me/bjtmastermind/hibernate_fabric/HibernateFabric.java
+++ src/main/java/me/bjtmastermind/hibernate_fabric/HibernateFabric.java
@@ -9,6 +9,7 @@ import me.bjtmastermind.hibernate_fabric.config.Config;
 import me.bjtmastermind.hibernate_fabric.world.ChunkUnloadHandler;
 import me.bjtmastermind.hibernate_fabric.world.TickEventHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 
 public class HibernateFabric implements ModInitializer {
@@ -30,8 +31,11 @@ public class HibernateFabric implements ModInitializer {
         TickEventHandler.register();
         ChunkUnloadHandler.register();
 
-        // Registers shutdown hook for cleanup
-        Runtime.getRuntime().addShutdownHook(new Thread(MemoryManager::shutdown));
+        // Registers shutdown hooks for cleanup
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+            MemoryManager.shutdown();
+            TickEventHandler.shutdown();
+        });
     }
 
     public static boolean isHibernating() {
